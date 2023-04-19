f83a04739a8280df01fca0d5dfb9ce97496774d9
BrayanAdair <brayanadairmontano083@gmail.com>
2023-04-18 22:24:13 -0600

    Commit Paginator en tablas
    Integracion de paginator en tablas de angular desde angular material

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
